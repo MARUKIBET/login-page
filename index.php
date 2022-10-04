9eb04cf560cadd20bdaf1c77ee9dc4e5af189b4a
MARU KIBET <98080885+MARUKIBET@users.noreply.github.com>
2022-10-04 10:34:00 +0300

    index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php
 
-  session_start();
+ session_start();
 ?>
 <head>
 	<meta charset="utf-8">
@@ -11,6 +11,7 @@
 	<!--font --->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="vendors/bootstrap.main.css">
+	<link rel="stylesheet" href="vendors/popper.min.js">
 	<link rel="stylesheet" href="style.css">
 </head>
 
@@ -46,28 +47,28 @@
 			   ?>
 			<div class="input-group">
 				<label for="full_name">Full name</label>
-				<input type="text" name="full_name" id="full_name" placeholder="Please Enter Full name">
+				<input type="text" name="full_name" id="full_name" placeholder="Please Enter Full name" value="<?php if(isset($_POST['full_name'])) echo $_POST['full_name']; ?>">
 				<i class="fas fa-exclamation-circle"></i>
 				<i class="fas fa-check-circle"></i>
 				<p>Error message</p>
 			</div>
 			<div class="input-group">
 				<label for="email">Email</label>
-				<input type="email" name="email" id="email" placeholder="Please Enter your email">
+				<input type="email" name="email" id="email" placeholder="Please Enter your email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>">
 				<i class="fas fa-exclamation-circle"></i>
 				<i class="fas fa-check-circle"></i>
 				<p>Error message</p>
 			</div>
 			<div class="input-group">
 				<label for="contact">Contact</label>
-				<input type="number" name="contact" id="contact" placeholder="Please Enter your phone number">
+				<input type="number" name="contact" id="contact" placeholder="Please Enter your phone number" value="<?php if(isset($_POST['contact'])) echo $_POST['contact']; ?>">
 				<i class="fas fa-exclamation-circle"></i>
 				<i class="fas fa-check-circle"></i>
 				<p>Error message</p>
 			</div>
 			<div class="input-group">
 				<label for="address">Address</label>
-				<textarea name="address" id="address" cols="0" rows="2" placeholder="Enter Your Address please"></textarea>
+				<textarea name="address" id="address" cols="0" rows="2" placeholder="Enter Your Address please" value="<?php if(isset($_POST['address'])) echo $_POST['address']; ?>"></textarea>
 				<i class="fas fa-exclamation-circle"></i>
 				<i class="fas fa-check-circle"></i>
 				<p>Error message</p>
